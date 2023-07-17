95c1292f1aa98df8a98a6e4c04d8458bd60f7953
duvanel <thiago.duvanel.ferreira@live.com>
2023-07-16 23:45:17 -0300

    Changes to permmit background running

--- fuzzers_table/open_openssl_server.sh
+++ fuzzers_table/open_openssl_server.sh
@@ -4,4 +4,4 @@ if [[ ! -f cert.pem ]] && [[ ! -f key.pem ]]; then
     openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem -subj /C=/ST=/L=/O=/OU=/CN=
 fi
 
-openssl s_server -key key.pem -dkey key.pem -cert cert.pem -dcert cert.pem
+openssl s_server -key key.pem -dkey key.pem -cert cert.pem -dcert cert.pem &
